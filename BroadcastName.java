a46f8f978e17d4daf2ae02e19197f9259a508941
Anurag <30341409+DeveloperAndCoder@users.noreply.github.com>
2019-03-23 01:11:20 +0530

    Add files via upload
    Initial Code

---
+++ BroadcastName.java
@@ -0,0 +1,23 @@
+import java.net.*;
+import java.io.*;
+
+public class BroadcastName {
+    private static DatagramSocket socket = null;
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your name!");
+        String name = br.readLine();
+        broadcast(name, InetAddress.getByName("255.255.255.255"));
+        System.out.println("Sent");
+    }
+ 
+    public static void broadcast(String broadcastMessage, InetAddress address) throws IOException {
+        socket = new DatagramSocket();
+        socket.setBroadcast(true);
+        byte[] buffer = broadcastMessage.getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4000);
+        socket.send(packet);
+        socket.close();
+    }
+}
